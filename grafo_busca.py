ef82cf4ee24cc739b60f898ea9a20f4a85bc64c8
João V. S. Cirilo <jvscirilo12@gmail.com>
2023-09-05 02:09:03 -0300

    rascunho removido

--- grafo_busca.py
+++
@@ -1,9 +0,0 @@
-from graph import Graph
-
-class SearchGraph(Graph):
-    def __init__(self, path):
-        super().__init__(path)
-
-    # TODO
-    def deph_search(self, v):
-        print(v, self.n)
