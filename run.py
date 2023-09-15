590820318b525a113d351a5c8d4daa272b40e67f
João Cirilo <jvscirilo12@gmail.com>
2023-09-15 19:14:22 -0300

    funções em util.py migradas para run.py

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
-from graph_depth import GraphDepth
-from graph_breadth import GraphBreadth
-from util import write_file_result
+from graph import Graph
+
+def write_search(colored_edges, path):
+    file = open(path, 'w', encoding='utf-8')
+    color_palette = ['0,0,0','0,0,255','255,0,0','255,255,0','0,255,0']
+    gsize = len(colored_edges)
+    file.write('nodedef>name VARCHAR,label VARCHAR\n')
+    for i in range(gsize):
+        file.write(f"{i+1},{i+1}\n")
+    file.write('edgedef>node1 VARCHAR,node2 VARCHAR,directed BOOLEAN,color VARCHAR\n')
+    for i in range(gsize):
+        for j in range(gsize):
+            color_code = colored_edges[i][j]
+            if color_code:
+                line = f"{i+1},{j+1},false,'{color_palette[color_code]}'\n"
+                file.write(line)
 
 for i in range(1, 21):
-    gpath = f"in/graph_{i}"
-    gd = GraphDepth(gpath)
-    gb = GraphBreadth(gpath)
-    write_file_formatted(gd, 0, f"run_result/graph_{i}_depth.gdf")
-    write_file_formatted(gb, 0, f"run_result/graph_{i}_breadth.gdf")
+    path = f"in/graph_{i}"
+    g = Graph(path)
+    print("graph_{}\tdiametro: {}\traio: {}".format(i, g.diameter(), g.radius()))
+    write_search(g.depth_search(0), f"out/graph_{i}_dfs.gdf")
+    write_search(g.breadth_search(0, only_lvl=False), f"out/graph_{i}_bfs.gdf")
