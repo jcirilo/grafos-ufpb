7cb427b9313ca2ebbf93c28a5dd7787bd532d23a
João Cirilo <jvscirilo12@gmail.com>
2023-09-18 22:38:10 -0300

    arquivo 'run.py' adaptado ao novo código

--- run.py
+++ run.py
@@ -1,23 +1,25 @@
 from graph import Graph
 
-def write_search(colored_edges, path):
-    file = open(path, 'w', encoding='utf-8')
-    color_palette = ['0,0,0','0,0,255','255,0,0','255,255,0','0,255,0']
-    gsize = len(colored_edges)
-    file.write('nodedef>name VARCHAR,label VARCHAR\n')
-    for i in range(gsize):
-        file.write(f"{i+1},{i+1}\n")
-    file.write('edgedef>node1 VARCHAR,node2 VARCHAR,directed BOOLEAN,color VARCHAR\n')
-    for i in range(gsize):
-        for j in range(gsize):
-            color_code = colored_edges[i][j]
-            if color_code:
-                line = f"{i+1},{j+1},false,'{color_palette[color_code]}'\n"
-                file.write(line)
+def write_out_file(colors, path):
+    n = len(colors)
+    f = open(file=path, mode='w', encoding='utf-8')
+    f.write("nodedef>name VARCHAR,label VARCHAR\n")
+    for i in range(1, n+1):
+        f.write(f"{i},{i}\n")
+    f.write("edgedef>node1 VARCHAR,node2 VARCHAR,directed BOOLEAN,color VARCHAR\n")
+    for i in range(n):
+        for j in range(n):
+            color = colors[i][j]
+            if color:
+                f.write(f"{i+1},{j+1},false,{color}\n")
 
-for i in range(1, 21):
-    path = f"in/graph_{i}"
-    g = Graph(path)
-    print("graph_{}\tdiametro: {}\traio: {}".format(i, g.diameter(), g.radius()))
-    write_search(g.depth_search(0), f"out/graph_{i}_dfs.gdf")
-    write_search(g.breadth_search(0, only_lvl=False), f"out/graph_{i}_bfs.gdf")
+def run():
+    for i in range(1, 21):
+        path = f"in/graph_{i}"
+        g = Graph(path)
+        dfs = g.search_dfs(0)
+        bfs = g.search_bfs(0)
+        write_out_file(colors=dfs, path=f"out/graph_{i}_dfs.gdf")
+        write_out_file(colors=bfs, path=f"out/graph_{i}_bfs.gdf")
+
+run()
