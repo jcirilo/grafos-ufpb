c59cc3aa9ef3d6b63bb0fca25e8905b7b778ed9f
João Cirilo <jvscirilo12@gmail.com>
2023-09-22 09:11:05 -0300

    modificação do retorno da busca em largura

--- graph_bfs.py
+++ graph_bfs.py
@@ -4,15 +4,15 @@ class GraphBFS(Graph):
     def __init__(self, path=None, data=None):
         super().__init__(path, data)
     
-    def search_bfs(self, v, get_lvl=False):
+    def search_bfs(self, v, get_colors=False):
         t = 0
         queue = list()
-        colors = list()
-        for c in range(self.n):
-            colors.append([None]*self.n)
         entry_t = [0]*self.n
         parent = [0]*self.n
         node_lvl = [None]*self.n
+        colors = list()
+        for c in range(self.n):
+            colors.append([None]*self.n)
         while 0 in entry_t:
             t += 1
             entry_t[v] = t
@@ -22,7 +22,7 @@ class GraphBFS(Graph):
                 v = queue.pop(0)
                 for w in self.opn_ngbhood(v):
                     if entry_t[w] == 0:
-                        color = self.color_edge(colors,v,w,"'0,0,255'")
+                        colors = self.color_edge(colors,v,w,"'0,0,255'")
                         parent[w] = v
                         node_lvl[w] = node_lvl[v]+1
                         t += 1
@@ -30,14 +30,14 @@ class GraphBFS(Graph):
                         queue.append(w)
                     elif node_lvl[w] == node_lvl[v]:
                         if parent[w] == parent[v]:
-                            color = self.color_edge(colors,v,w,"'255,0,0'")
+                            colors = self.color_edge(colors,v,w,"'255,0,0'")
                         else:
-                            color = self.color_edge(colors,v,w,"'255,255,0'")
+                            colors = self.color_edge(colors,v,w,"'255,255,0'")
                     elif node_lvl[w] == node_lvl[v]+1:
-                        color = self.color_edge(colors,v,w,"'0,255,0'")
-        if get_lvl:
-            return node_lvl
-        return colors
+                        colors = self.color_edge(colors,v,w,"'0,255,0'")
+        if get_colors:
+            return colors
+        return node_lvl
     
     def color_edge(self, m, v, w, color):
         if v < w:
